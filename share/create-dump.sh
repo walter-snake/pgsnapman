0e63e4d77d89708725c7622b2da3df8f80df2230
Walter Snake <meukacc@hotmail.com>
2017-12-16 09:25:32 +0100

    updated sql schema

--- share/create-dump.sh
+++ share/create-dump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pg_dump -Fc --no-owner --no-privileges pgsnapman > temp/pgsnapman.cdmp
-pg_dump -Fp --no-owner --no-privileges --schema-only gsnapman > share/pgsnapman.sql
+pg_dump -Fp --no-owner --no-privileges --schema-only pgsnapman > share/pgsnapman.sql
 exit $?
 
